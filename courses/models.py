5cdde23df297469d9a4deb628c3d522409cae0bd
Muhammed Recep Hava <66217824+mohammdhawa@users.noreply.github.com>
2024-10-08 12:44:33 +0300

    add static & media settings

--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Course(models.Model):
+    name = models.CharField(max_length=120)
+    description = models.TextField(max_length=5000)
+    price = models.FloatField()
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='courses/')
+
+    def __str__(self):
+        return self.name
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
